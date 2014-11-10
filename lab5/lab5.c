141e225e056015d500e41a7c34307fa05f1c9c83
Eric Chu <eric@ericchu.net>
2014-11-10 02:48:26 +0000

    Added input parsing
    Added header file

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "lab5.h"
 
 int main(int argc, char *argv[]) {
+    int n, b, p, c;
 
     if (argc != 5) {
         fprintf(stderr, "usage: %s <N> <B> <P> <C>\n", argv[0]);
     }
 
+    n = getint(argv[1]);
+    b = getint(argv[2]);
+    p = getint(argv[3]);
+    c = getint(argv[4]);
+
     return 0;
+}
+
+int getint(char str[]) {
+    char *endptr;
+    int i;
+
+    i = strtol(str, &endptr, 0);
+    if (endptr == str) {
+        fprintf(stderr, "strtol() failed\n");
+        exit(1);
+    }
+
+    return i;
 }
---
+++ lab5/lab5.h
@@ -0,0 +1,6 @@
+#ifndef LAB5_H
+#define LAB5_H
+
+int getint(char *str);
+
+#endif
