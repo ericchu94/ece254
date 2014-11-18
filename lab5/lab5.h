866d135e2523511298a65b974c4aa44434e3f19c
Eric Chu <eric@ericchu.net>
2014-11-18 16:40:52 +0000

    Added declarations in header

--- lab5/lab5.h
+++ lab5/lab5.h
@@ -10,9 +10,11 @@ int getint(char *str);
 void run();
 static void * produce(void *arg);
 static void * consume(void *arg);
+void push(int val);
 void create_thread(pthread_t *thread, void *(*start_routine) (void *), void *arg);
 void create_producers(struct thread producers[]);
 void create_consumers(struct thread consumers[]);
 void join_threads(struct thread threads[], int thread_count);
+void initialise_buffer();
 
 #endif
